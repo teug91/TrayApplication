0b8ab54458b089c87e1eaf386f280ec04efa9cda
Terje Uglebakken <teug91@gmail.com>
2017-10-15 16:21:16 +0200

    Initial commit.

--- TrayApplication/App.xaml.cs
+++ TrayApplication/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -9,9 +10,31 @@ using System.Windows;
 namespace TrayApplication
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    ///     Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        private TaskbarIcon notifyIcon;
+
+        /// <summary>
+        /// Creates tray icon and starts listening for input.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            notifyIcon = (TaskbarIcon)FindResource("NotifyIcon");
+        }
+
+        /// <summary>
+        /// Makes sure tray icon is removed when application exits.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnExit(ExitEventArgs e)
+        {
+            notifyIcon.Dispose();
+            base.OnExit(e);
+        }
     }
 }
